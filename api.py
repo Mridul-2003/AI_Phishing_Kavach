f9090a9127441c7c96b68e737e70c2db47a229e0
Mridul Mittal <mridulmittal2003@gmail.com>
2024-02-11 13:20:15 +0530

    api.py

---
+++ api.py
@@ -0,0 +1,24 @@
+import nltk
+nltk.download('punkt')
+import pickle
+from flask import Flask ,request,jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app,supports_credentials=True)
+
+model = pickle.load(open('/Users/useradmin/Desktop/AI_Phishing_Kavach/phishing.pkl','rb'))
+
+@app .route('/predict',methods=['POST'])
+def predict():
+    data = request.get_json()
+    prediction = model.predict([data['url']])
+    predicted = " "
+    if prediction[0]=='bad':
+        predicted+="This Link is Phishy Visit on your Own risk"
+    else:
+        predicted+="This Link is Safe to visit"
+    return jsonify({"predicted":predicted})
+
+if  __name__==("__main__"):
+    app.run(debug=True)
