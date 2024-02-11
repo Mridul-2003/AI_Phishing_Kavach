2b1ac7f9705fbf144d2bb0c527b241f310b04a53
Mridul Mittal <mridulmittal2003@gmail.com>
2024-02-11 14:00:24 +0530

    update api.py

--- api.py
+++ api.py
@@ -1,24 +1,38 @@
 import nltk
-nltk.download('punkt')
-import pickle
-from flask import Flask ,request,jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import pickle
+
+nltk.download('punkt')
 
 app = Flask(__name__)
-CORS(app,supports_credentials=True)
+CORS(app, supports_credentials=True)
 
-model = pickle.load(open('phishing.pkl','rb'))
+model = pickle.load(open('phishing.pkl', 'rb'))
 
-@app .route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    prediction = model.predict([data['url']])
-    predicted = " "
-    if prediction[0]=='bad':
-        predicted+="This Link is Phishy Visit on your Own risk"
-    else:
-        predicted+="This Link is Safe to visit"
-    return jsonify({"predicted":predicted})
+    try:
+        data = request.get_json()
+        urls = data.get('urls', []) 
 
-if  __name__==("__main__"):
+        if not urls:
+            raise ValueError("No 'urls' key found in the input JSON.")
+
+        predictions = []
+
+        for url in urls:
+            prediction = model.predict([url])
+            result = {
+                "url": url,
+                "predicted": "This Link is Phishy. Visit at your own risk." if prediction[0] == 'bad' else "This Link is Safe to visit."
+            }
+            predictions.append(result)
+
+        return jsonify({"predictions": predictions})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
     app.run(debug=True)
