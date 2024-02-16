7d7ba63f2d7bc8ae4192a0f45713585a5e32150e
Mridul Mittal <mridulmittal2003@gmail.com>
2024-02-16 12:38:13 +0530

    updates

--- popup.js
+++ popup.js
@@ -39,38 +39,48 @@ function checkURLFraud(url) {
         // Handle errors here
     });
 }
-const form = document.querySelector('form');
+// const form = document.querySelector('form');
 const phishyemail_form = document.querySelector('#phishy_email')
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
+// form.addEventListener('submit', async (event) => {
+//     event.preventDefault();
 
-    const inputUrl = document.getElementById('text').value;
+//     const inputUrl = document.getElementById('text').value;
 
-    try {
-        const response = await fetch('http://127.0.0.1:5000/predict_phishytext', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ text: [inputUrl] }),  // Sending an array even if it's just one URL
-        });
+//     try {
+//         const response = await fetch('http://127.0.0.1:5000/predict_phishytext', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({ text: [inputUrl] }),  // Sending an array even if it's just one URL
+//         });
 
-        if (response.ok) {
-            const result = (await response.json()).predictions[0].predicted;
-            const resultDiv = document.getElementById('prediction-result');
-            resultDiv.innerText = JSON.stringify(result, null, 2);  // Display the predictions as JSON
-        } else {
-            console.error('Request failed:', response.status);
-        }
-    } catch (error) {
-        console.error('Request failed:', error);
-    }
-});
+//         if (response.ok) {
+//             const result = (await response.json()).predictions[0].predicted;
+//             const resultDiv = document.getElementById('prediction-result');
+//             resultDiv.innerText = JSON.stringify(result, null, 2);  // Display the predictions as JSON
+//         } else {
+//             console.error('Request failed:', response.status);
+//         }
+//     } catch (error) {
+//         console.error('Request failed:', error);
+//     }
+// });
 
 phishyemail_form.addEventListener('submit', async (event) => {
     event.preventDefault();
-
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        let tab = tabs[0];
+        let url = tab.url; // Access the URL from the tab object
+        console.log('URL:', url);
+        console.log(tab)
+    });
     const inputUrl = document.getElementById('email').value;
+    const urlParams = new URLSearchParams(tab.url);
+    const emailParam = urlParams.get('email'); // Assuming 'email' is the parameter name
+    console.log('Email from URL:', emailParam);
+
+    console.log(inputUrl)
 
     try {
         const response = await fetch('http://127.0.0.1:5000/predict_phishyemail', {
@@ -96,7 +106,7 @@ phishyemail_form.addEventListener('submit', async (event) => {
 // popup.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    const cyberbullyingPredictionButton = document.getElementById('activeButton');
+    const cyberbullyingPredictionButton = document.getElementById('activeButton2');
     const predictionList = document.getElementById('prediction-list');
 
     cyberbullyingPredictionButton.addEventListener('click', function() {
